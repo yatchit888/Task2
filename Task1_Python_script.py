27299902c0e0ed91162de50d38345dd2c9f92cdc
Walker <111332019+WalkerChan1604@users.noreply.github.com>
2023-09-07 16:15:42 -0400

    Add files via upload

--- Task1_Python_script.py
+++ Task1_Python_script.py
@@ -1,7 +1,7 @@
 # Python script to scrape an article given the url of the article and store the extracted text in a file
 # Url: https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
 
-import os, sys
+import os
 import requests
 import re
 # Code here - Import BeautifulSoup library
@@ -13,7 +13,7 @@ def get_page():
 	global url
 	
 	# Code here - Ask the user to input "Enter url of a medium article: " and collect it in url
-	url= input("Enter url of a medium article:")
+	url = input("Enter url of a medium article: ")
 	# Code ends here
 	
 	# handling possible error
@@ -22,7 +22,7 @@ def get_page():
 		sys.exit(1)
 
 	# Code here - Call get method in requests object, pass url and collect it in res
-	res = requests.get(url)
+	res = requests.get('https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7')
 	# Code ends here
 
 	res.raise_for_status()
@@ -56,8 +56,9 @@ def save_file(text):
 	fname = f'scraped_articles/{name}.txt'
 	
 	# Code here - write a file using with (2 lines)
-	with open("./{}.txt".format(fname),"w") as f:
+	with open(fname,"w") as f:
 		f.write(text)
+	
 	# Code ends here
 
 	print(f'File saved in directory {fname}')
